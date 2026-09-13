91f4115711c62a84440652fb16a8d034d242ea55
Naufal <naufalfazil00@gmail.com>
2026-09-13 18:58:37 +0700

    fix: cegah hapus kategori yang masih digunakan

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import categoryRouter from "./routes/category/category.route";
 import postRouter from "./routes/post/post.route";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/categories", categoryRouter);
